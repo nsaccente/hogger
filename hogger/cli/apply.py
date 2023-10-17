d46c02d52260f40a785d5a86e9e4ee0c2c875302
nsaccente <32527006+nsaccente@users.noreply.github.com>
2023-10-17 19:58:04 -0400

    Finish rough draft of desired/actual resolution

--- hogger/cli/apply.py
+++ hogger/cli/apply.py
@@ -43,7 +43,9 @@ def apply(
         stack.callback(partial(print, "Releasing hoggerlock."))
 
         # Load all entities 
-        desired_states = {entity_code: {} for entity_code, _ in EntityCodes.items()}
+        desired_states: dict[int, dict[str, Entity]] = {
+            entity_code: {} for entity_code, _ in EntityCodes.items()
+        }
         for hoggerfile in get_hoggerpaths(dir_or_file):
             manifest = Manifest.from_file(hoggerfile)
             entities: list[Entity] = manifest.entities
@@ -54,7 +56,9 @@ def apply(
         
         # # Load entities from hoggerstate
         hoggerstates = wt.get_hoggerstate()
-        actual_states = {entity_code: {} for entity_code, _ in EntityCodes.items()}
+        actual_states: dict[int, dict[str, Entity]] = {
+            entity_code: {} for entity_code, _ in EntityCodes.items()
+        }
         for entity_code, hogger_identifier, db_key in hoggerstates:
             actual_states[entity_code][hogger_identifier] = (
                 wt.resolve_hoggerstate(
@@ -64,12 +68,47 @@ def apply(
                 )
             )
 
-
         # Compare actual and desired
-        # for entity_code in desired_states:
-        #     for actual_state in actual_states[entity_code]:
-        #         print(actual_state)
+        created : dict[int, dict[str, Entity]] = {
+            entity_code: {} for entity_code, _ in EntityCodes.items()
+        }
+        modified: dict[int, dict[str, Entity]] = {
+            entity_code: {} for entity_code, _ in EntityCodes.items()
+        }
+        for entity_code in EntityCodes:
+            for hogger_id, des_entity in desired_states[entity_code].items():
+                # If hogger_id from desired state exists in actual state,
+                # compute the diff; otherwise, add to `created`.
+                if hogger_id in actual_states[entity_code]:
+                    # If the diff returned has contents in it, add to
+                    # `modified`. Otherwise, no action necessary.
+                    entity_diff = des_entity.diff(
+                        actual_states[entity_code][hogger_id],
+                    )
+                    if len(entity_diff) > 0:
+                        modified[entity_code][hogger_id]
+                    del desired_states[entity_code][hogger_id]
+                else:
+                    created[entity_code][hogger_id] = des_entity
+        # Anything remaining in `actual_state` dict will be deleted.
+        deleted = actual_states
+
         
+        print()
+        print("To Be Created:")
+        for c in created[1]:
+            print(c)
+
+        print()
+        print("To Be Modified:")
+        for m in modified[1]:
+            print(m)
+
+        print()
+        print("To Be Deleted:")
+        for d in deleted[1]:
+            print(d)
+
         # Seek confirmation
 
         # Write
